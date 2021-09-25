9fbaffbb9d49f89c3adba47fec5509633f7e3ed7
Petra Kövesdi <kovesdi.petra@gmail.com>
2021-09-25 17:48:40 +0200

    Add formatting to remove edge borders.

--- Quoridor/src/components/Cell.jsx
+++ Quoridor/src/components/Cell.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
 export const Cell = (props) => {
+
+    let modulo = (props.cell+1) % 9;
+
+
+    let rightMost = modulo === 0 ? "rightMost" : "";
+
+    let lastRow = props.cell >= 72 ? "lastRow" : "";
+    
     return (
-        <div className="column" data-col={props.cell} data-player={props.player}>
+        <div className={`column ${rightMost} ${lastRow}`} data-col={props.cell} data-player={props.player}>
             <div className="cell"></div>
-            <div className="border horiz-border"></div>
+
+
             <div className="border vertic-border"></div>
+            <div className="border horiz-border"></div>
+
+            <div className="corner"></div>
+
+
+            
+        
         </div>
     )
 }
