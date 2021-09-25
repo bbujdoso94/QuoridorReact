acdfe5d0bbaaa0c0c2c5f6157be958802723a2b4
Petra Kövesdi <kovesdi.petra@gmail.com>
2021-09-25 16:42:57 +0200

    Add formatting.

--- Quoridor/src/components/Cell.jsx
+++ Quoridor/src/components/Cell.jsx
@@ -4,8 +4,8 @@ export const Cell = (props) => {
     return (
         <div className="column" data-col={props.cell} data-player={props.player}>
             <div className="cell"></div>
-            <div className="horiz-border"></div>
-            <div className="vertic-border"></div>
+            <div className="border horiz-border"></div>
+            <div className="border vertic-border"></div>
         </div>
     )
 }
