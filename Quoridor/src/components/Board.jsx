cdbc298ea7c0ff9ff6af8e6e36d3500694a08ccc
Petra Kövesdi <kovesdi.petra@gmail.com>
2021-09-25 15:03:52 +0200

    Place players on board and add eventlisteners to cells.

--- Quoridor/src/components/Board.jsx
+++ Quoridor/src/components/Board.jsx
@@ -8,10 +8,12 @@ export const Board = () => {
     for (let index = 0; index < 9; index++){
         let row = [];
         for (let col = 0; col < 9; col++){
-            row.push(col+1);
+            row.push(col+9*index);
         }
         myBoard.push(row);
     }
+
+    //4, 76
 
 
 
@@ -19,8 +21,16 @@ export const Board = () => {
         <div>
             My board
             {myBoard.map(x=>
-            <div className="row">{x.map(cell =>
-                <div className="column"></div>
+            <div className="row">{x.map(cell =>{
+                if(cell == 4){
+                    return <div data-col={cell} data-player="player1" className="column"></div>
+                }
+                if (cell == 76){
+                    return <div data-col={cell} data-player="player2" className="column"></div>
+                }
+                return <div onClick={x=>console.log({cell})} data-col={cell} data-player="empty" className="column"></div>
+                
+            }
             )}</div>
             )}
             
