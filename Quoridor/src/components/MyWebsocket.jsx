b60c17c35b8006b58cff944a4e8d5ca68546875f
Petra Kövesdi <kovesdi.petra@gmail.com>
2021-09-25 18:35:07 +0200

    Activate send function, change subscribe address to localhost.

--- Quoridor/src/components/MyWebsocket.jsx
+++ Quoridor/src/components/MyWebsocket.jsx
@@ -4,29 +4,29 @@ import Stomp from "webstomp-client";
 
 export const MyWebsocket = () => {
 
-    //let connected =false;
+    let connected =false;
     let socket ='';
     let stompClient = '';
-    // const  send = ()=> {
-    //       let send_message = 'hello !';
-    //       if (stompClient && stompClient.connected) {
-    //         const msg = { name: send_message };
-    //         stompClient.send("/app/hello", JSON.stringify(msg), {});
-    //       }
-    //     }
+    const  send = ()=> {
+           let send_message = 'My message!';
+           if (stompClient && stompClient.connected) {
+             const msg = { name: send_message };
+             stompClient.send("/app/hello", JSON.stringify(msg), {});
+           }
+         }
         const connect =()=> {
-          socket = new SockJS("https://dry-mountain-12518.herokuapp.com/gs-guide-websocket");
+          socket = new SockJS("http://127.0.0.1:8080/gs-guide-websocket");
           stompClient = Stomp.over(socket);
           stompClient.connect(
             {},
             frame => {
-              //connected = true;
+              connected = true;
               stompClient.subscribe("/topic/greetings", tick => {
               });
             },
             error => {
               console.log(error);
-              //connected = false;
+              connected = false;
             }
           );
         }
@@ -34,17 +34,18 @@ export const MyWebsocket = () => {
           if (stompClient) {
             stompClient.disconnect();
           }
-          //connected = false;
+          connected = false;
         }
-        // const tickleConnection =()=> {
-        //   connected ? disconnect() : connect();
-        // } 
+         const tickleConnection =()=> {
+           connected ? disconnect() : connect();
+         } 
 
     return (
         <div>
         Websocket component
         <button onClick={connect}>Connect</button>
         <button onClick={disconnect}>Disconnect</button>
+        <button onClick={send}>Send</button>
         </div>
     )
 }
