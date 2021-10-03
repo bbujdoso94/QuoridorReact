41bdabdbdcfcbcb7ae0c0bfe1c0befa5ccfc97c4
Bujdi <bujdi0604@gmail.com>
2021-10-03 06:01:50 -0700

    old Cell component removed, new renamed

--- Quoridor/src/components/Board.jsx
+++ Quoridor/src/components/Board.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect, useContext} from 'react';
-import { Cell } from './Cell';
 import { GameContext } from './GameContext';
-import Cell2 from './Cell2';
+import Cell from './Cell';
 
 export const Board = () => {
 
@@ -18,7 +17,7 @@ export const Board = () => {
     return (
         <div className="container">
             {boardState.map(celljson => {
-                return <Cell2 id={celljson.id} className={`${celljson.type} ${celljson.player} ${celljson.direction} ${celljson.wallType}`}></Cell2>
+                return <Cell id={celljson.id} className={`${celljson.type} ${celljson.player} ${celljson.direction} ${celljson.wallType}`}></Cell>
             })}
         </div>
 
--- Quoridor/src/components/Cell.jsx
+++ Quoridor/src/components/Cell.jsx
@@ -3,54 +3,16 @@ import MyWebsocket from './MyWebsocket';
 import {send} from './MyWebsocket';
 import { GameContext } from './GameContext';
 
+const Cell = (props) => {
 
-export const Cell = (props) => {
-    const [gameData, setGameData] = useContext(GameContext);
-
-    let modulo = (props.cell+1) % 9;
-
-    let rightMost = modulo === 0 ? true : false;
-
-    let lastRow = props.cell >= 72 ? true : false;
-    
+    // type :wall, stepField
+    // player: 0,1,2
+    // direction : horizontal, vertical, corner, none
+    // wallType: empty, solid, none
+    // id:
     return (
-        <React.Fragment>
-
-            { !rightMost       
-
-            ? !lastRow 
-            
-                ?
-
-                <div className={`column notRightEdge notLastRow`} data-col={props.cell} data-player={props.player}>
-                <div className="cell" onClick={() => send(props.cell)}></div>
-                <div className="border vertic-border"></div>
-                <div className="border horiz-border"></div>
-                <div className="corner"></div>
-                </div>
-
-                : 
-                    <div className={`column notRightEdge lastRow`} data-col={props.cell} data-player={props.player}>
-                    <div className="cell" onClick={()=>send(props.cell)}></div>
-                    <div className="border vertic-border"></div>
-                    </div>
-
-            :  !lastRow 
-            
-                ?
-
-                <div className={`column rightMost notLastRow`} data-col={props.cell} data-player={props.player}>            
-                <div className="cell" onClick={()=>send(props.cell)}></div>
-                <div className="border horiz-border"></div>
-                </div>
-                
-                :
-
-                <div className={`column rightMost lastRow`} data-col={props.cell} data-player={props.player}>
-                <div className="cell" onClick={()=>send(props.cell)}></div>
-                </div>
-        }
-
-        </React.Fragment>
+            <div id ={props.id} className={props.className}></div> 
     )
 }
+
+export default Cell;
