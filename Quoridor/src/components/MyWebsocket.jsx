3d46ae9bff0b5d8b1e802482d9ed7d6fe2515b21
Petra Kövesdi <kovesdi.petra@gmail.com>
2021-10-14 20:34:38 +0200

    Separate generate new game and join existing game.

--- Quoridor/src/components/MyWebsocket.jsx
+++ Quoridor/src/components/MyWebsocket.jsx
@@ -12,55 +12,71 @@ let gameId = 0;
 
 
 export const  send = (celldata)=> {
+  console.log("sending");
   let cellId = celldata;
   if (stompClient && stompClient.connected) {
     const msg = { 
       cellId: cellId,
       player:"player2"
     };
+
     console.log(msg);
     stompClient.send("/app/hello/" + gameId, JSON.stringify(msg), {});
   }
+  console.log("Message:");
 }
 export const MyWebsocket = () => {
 
-
   const setGameData = useContext(GameContext)[1];
 
-  const connect =()=> {
-    axios.get("http://127.0.0.1:8080/fetchNextGame")
-    .then(data =>{
-      console.log("fetching fetch endpoint")
-      console.log(data);
-    gameId = data.data.boardId;
-    socket = new SockJS("http://127.0.0.1:8080/gs-guide-websocket");
-    stompClient = Stomp.over(socket);
-    stompClient.connect(
+    const subscribeToEndpoint = () =>{
+      socket = new SockJS("http://127.0.0.1:8080/gs-guide-websocket");
+      stompClient = Stomp.over(socket);
+      console.log(stompClient);
+      
+      console.log("futok")
+      console.log(stompClient);
+      stompClient.connect(
       {},
       (frame) => {
         stompClient.subscribe("/topic/greetings/" + gameId, data => {
           console.log("DATA ITT ")
         console.log(data) 
         setGameData(JSON.parse(data.body));
-        })});
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  }
+        })},
+        error => {
+          console.log(error);
+        ;
+      })}
+      
 
-  const disconnect =()=> {
+  const createGame =()=> {
+    axios.get("http://127.0.0.1:8080/fetchNextGame")
+    .then(data =>{
+      console.log("fetching fetch endpoint")
+      console.log(data);
+    gameId = data.data.boardId;
+
+  }).then(()=>{subscribeToEndpoint()})}
+
+   const disconnect =()=> {
     if (stompClient) {
       stompClient.disconnect();
     }
+  }
+
+  function addGameId(inputGameId){
+    console.log(inputGameId);
+    gameId = inputGameId;
   }
 
   return (
       <>
       <div>
       Websocket component
-      <button onClick={connect}>Connect</button>
+      <button onClick={createGame}>Create Game</button><br/>
+      <input onChange={(e)=>addGameId(e.target.value)}></input>
+      <button onClick={subscribeToEndpoint}>Join Game</button>
       <button onClick={disconnect}>Disconnect</button> 
       <br></br>
       <Board></Board>
