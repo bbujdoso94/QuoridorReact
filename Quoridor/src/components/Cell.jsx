c9e41e1115272973cd62d54a660dcc358d64e475
Petra Kövesdi <kovesdi.petra@gmail.com>
2021-10-21 23:18:45 +0200

    Implement placement of wall, adjust highlight accordingly.

--- Quoridor/src/components/Cell.jsx
+++ Quoridor/src/components/Cell.jsx
@@ -4,29 +4,44 @@ import {send} from './MyWebsocket';
 const Cell = (props) => {
 
     function MouseOver(event) {
-        if (props.className.includes("corner")){
+        if (props.className.includes("corner")
+            || props.className.includes("reserved")){
             return;
         }
-        event.target.style.boxSizing="border-box";
-        event.target.style.border = "4px solid yellow";
+        let adjacentCorner = null;
+        let adjacentWall = null;
+
         if (props.className.includes("wall")
             && props.className.includes("horizontal")){
-            let adjacentCorner = document.getElementById(`${props.id + 1}`);
-            adjacentCorner.style.boxSizing="border-box";
-            adjacentCorner.style.border = "4px solid yellow";
-            let adjacentWall = document.getElementById(`${props.id + 2}`);
-            adjacentWall.style.boxSizing="border-box";
-            adjacentWall.style.border = "4px solid yellow";
+            
+            adjacentCorner = document.getElementById(`${props.id + 1}`);
+            adjacentWall = document.getElementById(`${props.id + 2}`);
+
         }
         if (props.className.includes("wall")
         && props.className.includes("vertical")){
-        let adjacentCorner = document.getElementById(`${props.id + 17}`);
-        adjacentCorner.style.boxSizing="border-box";
-        adjacentCorner.style.border = "4px solid yellow";
-        let adjacentWall = document.getElementById(`${props.id + 34}`);
-        adjacentWall.style.boxSizing="border-box";
-        adjacentWall.style.border = "4px solid yellow";
+        
+            adjacentCorner = document.getElementById(`${props.id + 17}`);
+            adjacentWall = document.getElementById(`${props.id + 34}`);
+        
         }
+
+        if (props.className.includes("stepField")){
+            event.target.style.boxSizing="border-box";
+            event.target.style.border = "4px solid yellow";
+        }
+        if (props.className.includes("wall")){
+            if ( ! adjacentCorner.classList.contains("reserved")
+                && ! adjacentWall.classList.contains("reserved")){
+                event.target.style.boxSizing="border-box";
+                event.target.style.border = "4px solid yellow";
+                adjacentCorner.style.boxSizing="border-box";
+                adjacentCorner.style.border = "4px solid yellow";
+                adjacentWall.style.boxSizing="border-box";
+                adjacentWall.style.border = "4px solid yellow";
+                }
+        }
+        
       }
 
 
@@ -35,28 +50,26 @@ const Cell = (props) => {
         event.target.style.boxSizing="";
         event.target.style.border = "";
         if (props.className.includes("wall")
+        && ! props.className.includes("reserved")
         && props.className.includes("horizontal")){
             let adjacent = document.getElementById(`${props.id + 1}`);
-            adjacent.style.boxSizing="";
             adjacent.style.border = "";
             let adjacentWall = document.getElementById(`${props.id + 2}`);
-            adjacentWall.style.boxSizing="";
             adjacentWall.style.border = "";
         }
         if (props.className.includes("wall")
+        && ! props.className.includes("reserved")
         && props.className.includes("vertical")){
         let adjacentCorner = document.getElementById(`${props.id + 17}`);
-        adjacentCorner.style.boxSizing="";
         adjacentCorner.style.border = "";
         let adjacentWall = document.getElementById(`${props.id + 34}`);
-        adjacentWall.style.boxSizing="";
         adjacentWall.style.border = "";
         }
       }
 
     
     return (
-            <div onMouseOver={MouseOver} onMouseOut={MouseOut} onClick={() => send(props.id)} id ={props.id} className={props.className}></div> 
+            <div onMouseOver={MouseOver} onMouseOut={MouseOut} onClick={(event) => {send(props.id);MouseOut(event)}} id ={props.id} className={props.className}></div> 
     )
 }
 
