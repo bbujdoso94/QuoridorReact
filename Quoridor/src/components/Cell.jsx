d26080adc8d236518ec3be54360df82bd93354e7
Petra Kövesdi <kovesdi.petra@gmail.com>
2021-10-21 21:04:48 +0200

    Add highlight to each cell when mouse hovers above cell.

--- Quoridor/src/components/Cell.jsx
+++ Quoridor/src/components/Cell.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import {send} from './MyWebsocket';
 
 const Cell = (props) => {
+
+    function MouseOver(event) {
+        event.target.style.boxSizing="border-box";
+        event.target.style.border = "4px solid yellow";
+      }
+      function MouseOut(event){
+        event.target.style.boxSizing="";
+        event.target.style.border = "";
+      }
+
     
     return (
-            <div onClick={() => send(props.id)} id ={props.id} className={props.className}></div> 
+            <div onMouseOver={MouseOver} onMouseOut={MouseOut} onClick={() => send(props.id)} id ={props.id} className={props.className}></div> 
     )
 }
 
